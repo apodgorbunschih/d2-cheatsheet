ae7a2d1de6c1655a4cd914c8621245b30cc39926
Alex Podgorbunschih <alex.podgorbunschih@gmail.com>
2024-05-07 16:27:22 +0200

    fix the delimiter for nodes

--- main.go
+++ main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"context"
 	"os"
 	"oss.terrastruct.com/d2/d2compiler"
 	"oss.terrastruct.com/d2/d2format"
-	"oss.terrastruct.com/d2/d2lib"
 	"path/filepath"
 	"strings"
 )
@@ -13,7 +11,6 @@ import (
 func main() {
 	emptyCanvas()
 	twoNodes()
-	twoNodesUsingD2lib()
 }
 
 func emptyCanvas() {
@@ -30,7 +27,7 @@ func emptyCanvas() {
 
 func twoNodes() {
 	// Create a graph/canvas with 2 Nodes
-	graph, _, _ := d2compiler.Compile("", strings.NewReader("A,B"), nil)
+	graph, _, _ := d2compiler.Compile("", strings.NewReader("A;B"), nil)
 
 	// Get the Abstract Syntax Tree
 	ast := d2format.Format(graph.AST)
@@ -38,18 +35,4 @@ func twoNodes() {
 
 	// Save it to a d2 file
 	_ = os.WriteFile(filepath.Join("results/twoNodes.d2"), data, 0600)
-}
-
-func twoNodesUsingD2lib() {
-	ctx := context.Background()
-
-	// Create a graph/canvas with 2 Nodes
-	_, graph, _ := d2lib.Compile(ctx, "A,B", nil, nil)
-
-	// Get the Abstract Syntax Tree
-	ast := d2format.Format(graph.AST)
-	data := []byte(ast)
-
-	// Save it to a d2 file
-	_ = os.WriteFile(filepath.Join("results/twoNodesUsingD2lib.d2"), data, 0600)
 }
