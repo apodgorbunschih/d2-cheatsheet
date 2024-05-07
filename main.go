f2cd24db46dc9e52e16eee6d17003b5c4b1caafa
Alex Podgorbunschih <alex.podgorbunschih@gmail.com>
2024-05-07 15:25:07 +0200

    feature: Add example for empty canvas and two nodes

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"os"
+	"oss.terrastruct.com/d2/d2compiler"
+	"oss.terrastruct.com/d2/d2format"
+	"path/filepath"
+	"strings"
+)
+
+func main() {
+	emptyCanvas()
+	twoNodes()
+}
+
+func emptyCanvas() {
+	// Create an empty graph/canvas
+	graph, _, _ := d2compiler.Compile("", strings.NewReader(""), nil)
+
+	// Get the Abstract Syntax Tree
+	ast := d2format.Format(graph.AST)
+	data := []byte(ast)
+
+	// Save it to a d2 file
+	_ = os.WriteFile(filepath.Join("emptyCanvas.d2"), data, 0600)
+}
+
+func twoNodes() {
+	// Create an empty graph/canvas with 2 Nodes
+	graph, _, _ := d2compiler.Compile("", strings.NewReader("A,B"), nil)
+
+	// Get the Abstract Syntax Tree
+	ast := d2format.Format(graph.AST)
+	data := []byte(ast)
+
+	// Save it to a d2 file
+	_ = os.WriteFile(filepath.Join("results/twoNodes.d2"), data, 0600)
+}
