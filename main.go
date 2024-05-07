0ca2e9d3c4c9f89e98cc0f0c99ebaf5a7717113f
Alex Podgorbunschih <alex.podgorbunschih@gmail.com>
2024-05-07 16:24:48 +0200

    add d2lib example

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"context"
 	"os"
 	"oss.terrastruct.com/d2/d2compiler"
 	"oss.terrastruct.com/d2/d2format"
+	"oss.terrastruct.com/d2/d2lib"
 	"path/filepath"
 	"strings"
 )
@@ -11,6 +13,7 @@ import (
 func main() {
 	emptyCanvas()
 	twoNodes()
+	twoNodesUsingD2lib()
 }
 
 func emptyCanvas() {
@@ -26,7 +29,7 @@ func emptyCanvas() {
 }
 
 func twoNodes() {
-	// Create an empty graph/canvas with 2 Nodes
+	// Create a graph/canvas with 2 Nodes
 	graph, _, _ := d2compiler.Compile("", strings.NewReader("A,B"), nil)
 
 	// Get the Abstract Syntax Tree
@@ -35,4 +38,18 @@ func twoNodes() {
 
 	// Save it to a d2 file
 	_ = os.WriteFile(filepath.Join("results/twoNodes.d2"), data, 0600)
+}
+
+func twoNodesUsingD2lib() {
+	ctx := context.Background()
+
+	// Create a graph/canvas with 2 Nodes
+	_, graph, _ := d2lib.Compile(ctx, "A,B", nil, nil)
+
+	// Get the Abstract Syntax Tree
+	ast := d2format.Format(graph.AST)
+	data := []byte(ast)
+
+	// Save it to a d2 file
+	_ = os.WriteFile(filepath.Join("results/twoNodesUsingD2lib.d2"), data, 0600)
 }
